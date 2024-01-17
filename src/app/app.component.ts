57fdcace16071d1f2b65b6d2e9beba5a37ddb511
tom hikari <tomprescott1565@gmail.com>
2024-01-17 12:58:47 +0100

    Update header and food components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ng-food-app';
+  foods: {
+    name: string;
+    price: number;
+    description: string;
+    imgUrl: string;
+  }[] = [
+    {
+      name: 'Double Burger',
+      description:
+        'Double Burger Sandwiches, Frites & Pepsi, un repas gourmand et complet qui vous fera voyager. Vous pourrez déguster un burger au choix entre bœuf, poulet ou poisson, garni de fromage fondu, de salade croquante et de sauce au choix.',
+      price: 9.72,
+      imgUrl: 'https://intrax.wakafoodz.com/storage/1521_287_1.png',
+    },
+    {
+      name: 'Big Bite Meal',
+      description:
+        'Trois délicieux morceaux de poulet croustillant, accompagnés d’une portion généreuse de frites dorées à souhait, une salade de chou fraîche et croquante, un riz moelleux et parfumé, un pain tout juste sorti du four, encore tiède, une sauce à l’ail délicieusement relevée, et un Coca glacé qui pétille de fraîcheur.',
+      price: 15.24,
+      imgUrl: 'https://intrax.wakafoodz.com/storage/Big%20bite%20meal.jpg',
+    },
+    {
+      name: 'Family Bite',
+      description:
+        'Cinq savoureuses pièces de poulet croustillant, accompagnées de cinq tendres blancs de poulet, deux Zinger/filet pour une explosion de saveurs, trois Twist pour une touche épicée, une grande portion de frites dorées, trois pains moelleux, et trois Coca glacés qui pétillent de fraîcheur.',
+      price: 54.96,
+      imgUrl: 'https://intrax.wakafoodz.com/storage/Family%20bite.jpg',
+    },
+  ];
 }
--- src/app/food/food.component.ts
+++ src/app/food/food.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-food',
   templateUrl: './food.component.html',
-  styleUrl: './food.component.scss'
+  styleUrl: './food.component.scss',
 })
-export class FoodComponent {
+export class FoodComponent implements OnInit {
+  @Input() food: {
+    name: string;
+    price: number;
+    description: string;
+    imgUrl: string;
+  };
 
+  @Output() foodSelectedEvent = new EventEmitter<{
+    name: string;
+    price: number;
+    description: string;
+    imgUrl: string;
+  }>();
+
+  foodSelected: boolean = false;
+  quantity: number = 1;
+  initialPrice: number;
+
+  ngOnInit(): void {
+    this.initialPrice = this.food.price;
+  }
+
+  onIncrement() {
+    this.quantity++;
+    this.updateFoodPrice();
+  }
+
+  onDecrement() {
+    if (this.quantity > 1) {
+      this.quantity--;
+      this.updateFoodPrice();
+    }
+  }
+
+  updateFoodPrice() {
+    this.food.price = Math.floor(this.initialPrice * this.quantity);
+  }
+
+  onSelectFood() {
+    this.foodSelected = !this.foodSelected;
+    this.foodSelectedEvent.emit(this.food);
+  }
 }
