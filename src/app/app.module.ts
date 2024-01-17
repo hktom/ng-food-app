f28b4cd2d135ad84810a923bb8e041942a67f422
tom hikari <tomprescott1565@gmail.com>
2024-01-17 12:30:01 +0100

    Add Bootstrap CSS to styles.scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { CartComponent } from './cart/cart.component';
+import { FoodComponent } from './food/food.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    CartComponent,
+    FoodComponent,
+    FooterComponent,
+    HeaderComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule
